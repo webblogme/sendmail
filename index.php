926f68b6d177268cf39355aa1aaec283cf686253
webblogme <webblogme@gmail.com>
2018-09-21 11:43:51 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="./vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="./vendor/twbs/bootstrap/dist/css/bootstrap-theme.min.css">
+
+</head>
+<body>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+
+				<h1>
+					Email tester tool
+				</h1>
+
+				<p>
+
+					<a href="https://myaccount.google.com/lesssecureapps?pli=1" target="_blank">Enable less secure for Gmail</a> |
+					<a href="https://mailtrap.io" target="_blank">Mailtrap</a> |
+					<a href="vendor/phpmailer/phpmailer/examples" target="_blank">View docs</a>
+
+				</p>
+
+
+				<h3>
+					Test via hosting smtp
+				</h3>
+
+				<form action="sendmail.php" method="post" class="form-horizontal">
+
+					<div class="form-group"><label class="col-md-4 control-label" for="checkboxes">SMTP server</label><div class="col-md-4"><input type="text" value="" name="from" placeholder="yourhosting.com" class="form-control input-md"></div></div>
+					<div class="form-group"><label class="col-md-4 control-label" for="checkboxes">Port</label><div class="col-md-4"><input type="text" value="" name="from" placeholder="26" class="form-control input-md"></div></div>
+					<div class="form-group"><label class="col-md-4 control-label" for="checkboxes">From email</label><div class="col-md-4"><input type="text" value="" name="from" placeholder="@image-asia.com" class="form-control input-md"></div></div>
+					<div class="form-group"><label class="col-md-4 control-label" for="checkboxes">Password</label><div class="col-md-4"><input type="password" value="" name="password" placeholder="password" class="form-control input-md"></div></div>
+					<div class="form-group"><label class="col-md-4 control-label" for="checkboxes">To email</label><div class="col-md-4"><input type="text" value="" name="to" placeholder="recipent" class="form-control input-md"></div></div>
+					<div class="form-group"><label class="col-md-4 control-label" for="checkboxes">Subject</label><div class="col-md-4"><input type="text" value="test subject" name="subject" placeholder="your subject" class="form-control input-md"></div></div>
+					<!-- Textarea -->
+					<div class="form-group">
+					<label class="col-md-4 control-label" for="textarea">Body</label>
+					<div class="col-md-4">
+						<textarea class="form-control" id="textarea1" name="message">Lorem ipsum</textarea>
+					</div>
+					</div>
+
+
+					<div class="form-group">
+					<label class="col-md-4 control-label" for="checkboxes"></label>
+						<div class="col-md-4">
+							<label class="checkbox-inline" for="checkboxes-0">
+							<input type="checkbox" name="debug" id="checkboxes-0" value="1">
+							Enable debug
+							</label>
+						</div>
+					</div>
+
+					<div class="form-group"><label class="col-md-4 control-label" for="checkboxes"></label><div class="col-md-4"><button id="singlebutton1" name="singlebutton" class="btn btn-primary">Send</button></div></div>
+
+				</form>
+
+
+				<h3>
+					Test via Gmail smtp
+				</h3>
+				<form action="sendmail.php" method="post" class="form-horizontal">
+					<input type="hidden" name="smtp" value="smtp.gmail.com">
+					<input type="hidden" name="port" value="587">
+					<div class="form-group"><label class="col-md-4 control-label" for="checkboxes">From email</label><div class="col-md-4"><input type="text" value="" name="from" placeholder="@gmail.com" class="form-control input-md"></div></div>
+					<div class="form-group"><label class="col-md-4 control-label" for="checkboxes">Password</label><div class="col-md-4"><input type="password" value="" name="password" placeholder="password" class="form-control input-md"></div></div>
+					<div class="form-group"><label class="col-md-4 control-label" for="checkboxes">To email</label><div class="col-md-4"><input type="text" value="" name="to" placeholder="recipent" class="form-control input-md"></div></div>
+					<div class="form-group"><label class="col-md-4 control-label" for="checkboxes">Subject</label><div class="col-md-4"><input type="text" value="test subject" name="subject" placeholder="your subject" class="form-control input-md"></div></div>
+					<!-- Textarea -->
+					<div class="form-group">
+					<label class="col-md-4 control-label" for="textarea">Body</label>
+					<div class="col-md-4">
+						<textarea class="form-control" id="textarea2" name="message">Lorem ipsum</textarea>
+					</div>
+					</div>
+
+					<div class="form-group">
+					<label class="col-md-4 control-label" for="checkboxes"></label>
+						<div class="col-md-4">
+							<label class="checkbox-inline" for="checkboxes-0">
+							<input type="checkbox" name="debug" id="checkboxes-0" value="1">
+							Enable debug
+							</label>
+						</div>
+					</div>
+
+					<div class="form-group"><label class="col-md-4 control-label" for="checkboxes"></label><div class="col-md-4"><button id="singlebutton1" name="singlebutton" class="btn btn-primary">Send</button></div></div>
+
+				</form>
+
+
+				<h3>
+					Test via Mail trap smtp
+				</h3>
+				<form action="sendmail.php" method="post" class="form-horizontal">
+					<input type="hidden" name="smtp" value="smtp.mailtrap.io"><!-- Check again with mailtrap -->
+					<input type="hidden" name="port" value="2525"><!-- Check again with mailtrap -->
+					<div class="form-group"><label class="col-md-4 control-label" for="checkboxes">From email</label><div class="col-md-4"><input type="text" value="" name="from" placeholder="@yourwebsite.com" class="form-control input-md"></div></div>
+					<div class="form-group"><label class="col-md-4 control-label" for="checkboxes">Password</label><div class="col-md-4"><input type="password" value="" name="password" placeholder="password" class="form-control input-md"></div></div>
+					<div class="form-group"><label class="col-md-4 control-label" for="checkboxes">To email</label><div class="col-md-4"><input type="text" value="" name="to" placeholder="recipent" class="form-control input-md"></div></div>
+					<div class="form-group"><label class="col-md-4 control-label" for="checkboxes">Subject</label><div class="col-md-4"><input type="text" value="test subject" name="subject" placeholder="your subject" class="form-control input-md"></div></div>
+					<!-- Textarea -->
+					<div class="form-group">
+					<label class="col-md-4 control-label" for="textarea">Body</label>
+					<div class="col-md-4">
+						<textarea class="form-control" id="textarea3" name="message">Lorem ipsum</textarea>
+					</div>
+					</div>
+
+					<div class="form-group">
+					<label class="col-md-4 control-label" for="checkboxes"></label>
+						<div class="col-md-4">
+							<label class="checkbox-inline" for="checkboxes-0">
+							<input type="checkbox" name="debug" id="checkboxes-0" value="1">
+							Enable debug
+							</label>
+						</div>
+					</div>
+
+					<div class="form-group"><label class="col-md-4 control-label" for="checkboxes"></label><div class="col-md-4"><button id="singlebutton1" name="singlebutton" class="btn btn-primary">Send</button></div></div>
+
+				</form>
+			</div>
+			<!-- /.col -->
+		</div>
+		<!-- /.div -->
+	</div>
+	<!-- /.container -->
+
+<script src="/vendor/tinymce/tinymce/jquery.tinymce.min.js"></script>
+<script src="/vendor/tinymce/tinymce/tinymce.min.js"></script>
+<script type="text/javascript">
+    tinymce.init({
+
+        selector: "#textarea1, #textarea2, #textarea3",
+        editor_encoding: "raw",
+        height : "300px",
+        plugins: "paste",
+        paste_as_text: true,
+
+        plugins: [
+            "preview charmap",
+            "code fullscreen",
+            "template paste",
+            "wordcount"
+        ],
+
+        toolbar1: "undo redo | cut copy paste",
+        toolbar2: "bold italic underline superscript | charmap | removeformat | code fullscreen preview",
+
+        menubar: false,
+        statusbar:true,
+        toolbar_items_size: 'small',
+        language : 'en',
+
+    });
+
+</script>
+
+
+</body>
+</html>
---
+++ sendmail.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+
+<?php
+//require 'vendor/phpmailer/phpmailer/class.phpmailer.php';
+require 'vendor/Autoload.php';
+
+/**
+ * This example shows making an SMTP connection with authentication.
+ */
+
+//SMTP needs accurate times, and the PHP time zone MUST be set
+//This should be done in your php.ini, but this is how to do it if you don't have access to that
+date_default_timezone_set('Etc/UTC');
+
+$from     = isset($_POST['from']) ? $_POST['from']        : '';
+$password = isset($_POST['password']) ? $_POST['password']: '';
+$to       = isset($_POST['to']) ? $_POST['to']            : '';
+$smtp     = isset($_POST['smtp']) ? $_POST['smtp']        : '';
+$port     = isset($_POST['port']) ? $_POST['port']        : '';
+$message  = isset($_POST['message']) ? $_POST['message']  : '';
+$subject  = isset($_POST['subject']) ? $_POST['subject'] : '';
+$enable_debug=isset($_POST['enable_debug']) ? $_POST['enable_debug'] : '';
+
+
+/*
+Less secure app access NEEDED.
+
+Some apps and devices use less secure sign-in technology, which makes your account more vulnerable. You can turn off access for these apps, which we recommend, or turn on access if you want to use them despite the risks.
+*/
+
+require 'vendor/autoload.php';
+$mail = new PHPMailer;
+
+//Enable SMTP debugging
+// 0               = off (for production use)
+// 1               = client messages
+// 2               = client and server messages
+$mail->SMTPDebug   = ($enable_debug == 1 ? 2 : 0);
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
+
+$mail->IsSMTP();                                      // Set mailer to use SMTP
+$mail->Host       = $smtp;                 // Specify main and backup server
+$mail->Port       = $port;                                    // Set the SMTP port
+$mail->SMTPAuth   = true;                               // Enable SMTP authentication
+$mail->Username   = $from;                // SMTP username
+$mail->Password   = $password;                  // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
+
+$mail->From       = $from;
+$mail->FromName   = $from;
+$mail->AddAddress($to, $to);  // Add a recipient
+//$mail->AddAddress('ellen@example.com');               // Name is optional
+
+$mail->IsHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = $subject;
+$mail->Body    = $message;
+$mail->AltBody = strip_tags($message);
+
+echo '<br>';
+echo '<br>';
+echo '<br><center>';
+
+if(!$mail->Send()) {
+
+   echo 'Message could not be sent.';
+   echo 'Mailer Error: ' . $mail->ErrorInfo;
+   echo '</center>';
+   exit;
+}
+
+echo 'Message has been sent';
+echo '</center>';
+
+?>
+
+</body>
+</html>
